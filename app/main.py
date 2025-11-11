3e488d520f1d4a15d6714281053b2c60c8ab023a
AMIT <amitaaranya@gmail.com>
2025-11-11 11:46:14 +0530

    Added CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.settings.config import TITLE, VERSION
 from app.routes import *
-
 
 def initialize_application():
     app = FastAPI(
         title=TITLE,
         version=VERSION
     )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     app.include_router(common_rt)
     app.include_router(user_rt)
     app.include_router(agent_rt)
