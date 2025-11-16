a18b913e3e25412cff25aca7632bf4630aabddce
AMIT <amitaaranya@gmail.com>
2025-11-16 13:25:36 +0530

    Used common service account

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -7,4 +7,4 @@ __all__ = ["db", "storage", "firebase"]
 
 db = FirestoreManager(ENV.FIRE_STORE_DB_NAME, ENV.GOOGLE_CREDENTIAL_PATH)
 storage = StorageManager(credential_path=ENV.GOOGLE_CREDENTIAL_PATH)
-firebase = FirebaseManager(ENV.FIREBASE_CREDENTIAL_PATH)
+firebase = FirebaseManager(ENV.GOOGLE_CREDENTIAL_PATH)
